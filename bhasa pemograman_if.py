6b56aee42293c85142698c18c79bb2180d30fbef
stevanidean <92729505+stevanidean@users.noreply.github.com>
2021-11-15 21:14:44 +0700

    Add files via upload

---
+++ bhasa pemograman_if.py
@@ -0,0 +1,11 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+num = input("3")
+3
+num = input("5")
+5
+num = input("10")
+10
+num = input("0.05883021198263216")
+0.05883021198263216
+
